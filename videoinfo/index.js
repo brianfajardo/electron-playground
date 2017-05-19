2d533d25a66cab81903b27f0d89c081e4287f2d5
Brian Fajardo <bmf5@hotmail.com>
2017-05-18 21:50:34 -0400

    Add IPC communication between BrowserWindow and Electron app

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,3 +1,18 @@
-const { app } = require('electron')
+// Electron runs in Node therefore uses CommonJS
+const { app, BrowserWindow, ipcMain } = require('electron')
+const ffmpeg = require('fluent-ffmpeg')
 
-app.on('ready', () => console.log('Electron is now ready!'))
+// Global mainWindow
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow()
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+// Receiving an event from ipcRenderer from BrowserWindow
+ipcMain.on('video:submit', (e, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('video:duration', metadata.format.duration)
+  })
+})
