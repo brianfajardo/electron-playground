c193aaed1d80166a0be85dd13a49c6e19ca3b881
Brian Fajardo <bmf5@hotmail.com>
2017-05-19 12:19:17 -0400

    Add menubar labels compatible with both Windows and macOS

--- todos/index.js
+++ todos/index.js
@@ -1,9 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 let mainWindow
 
 app.on('ready', () => {
-  console.log('Electron is ready')
+  console.log('Electron is ready!')
   mainWindow = new BrowserWindow()
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu)
 })
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'Add New Todo' },
+      {
+        label: 'Exit',
+        click() {
+          app.quit()
+        }
+      }
+    ]
+  }
+]
+
+// Mac menu compatibility
+// Create a new obj @ the start of menuTemplate array
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({})
+}
