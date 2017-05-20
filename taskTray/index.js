e4248e41f755e7ca1280cc6a86cd7e2e23698fc8
Brian Fajardo <bmf5@hotmail.com>
2017-05-20 12:00:12 -0400

    Toggle window visibility on tray click

--- taskTray/index.js
+++ taskTray/index.js
@@ -5,15 +5,23 @@ let mainWindow
 let tray
 
 app.on('ready', () => {
+  // Main Window
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
     resizable: false,
+    show: false
   })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 
+
+  // Tray
   const icon = process.platform === 'darwin' ? 'iconTray_mac.png' : 'iconTray_windows.png'
   const iconPath = path.join(__dirname, `./src/assets/${icon}`)
   tray = new Tray(iconPath)
+  tray.on('click', (e, bounds) => {
+    console.log(bounds.x, bounds.y)
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  })
 })
