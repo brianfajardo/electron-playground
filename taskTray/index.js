906d1401600261fd64d2d0479e185353a71e21d3
Brian Fajardo <bmf5@hotmail.com>
2017-05-22 12:07:17 -0400

    Possible bug with Electron setBounds configuration settings

--- taskTray/index.js
+++ taskTray/index.js
@@ -19,9 +19,34 @@ app.on('ready', () => {
   // Tray
   const icon = process.platform === 'darwin' ? 'iconTray_mac.png' : 'iconTray_windows.png'
   const iconPath = path.join(__dirname, `./src/assets/${icon}`)
+
+
   tray = new Tray(iconPath)
   tray.on('click', (e, bounds) => {
-    console.log(bounds.x, bounds.y)
-    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    // CLick event bounds
+    const { x, y } = bounds
+    // Window height & width
+    const { height, width } = mainWindow.getBounds()
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      const yPosition = process.platform === 'darwin' ? y : (y - height)
+      const boundsConfig = {
+        x: Math.floor(x - (width / 2)),
+        y: yPosition,
+        height,
+        width,
+      }
+      console.log(boundsConfig)
+      mainWindow.setBounds(boundsConfig)
+      // mainWindow.setBounds({
+      //   x: x - (width / 2),
+      //   y: yPosition,
+      //   height,
+      //   width
+      // })
+      mainWindow.show()
+    }
   })
 })
